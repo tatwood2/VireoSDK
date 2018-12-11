9b936d66d381be40082e77c55d74b5cefa072127
Vireo Bot (from Travis CI) <vireobot@users.noreply.github.com>
2018-12-11 14:29:53 -0600

    Deploy ni/VireoSDK to github.com/ni/VireoSDK.git:gh-pages

--- search/classes_12.js
+++ search/classes_12.js
@@ -1,8 +1,6 @@
 var searchData=
 [
   ['vararginstruction',['VarArgInstruction',['../struct_vireo_1_1_var_arg_instruction.html',1,'Vireo']]],
-  ['variantattributemanager',['VariantAttributeManager',['../class_vireo_1_1_variant_attribute_manager.html',1,'Vireo']]],
-  ['varianttodataparamblock',['VariantToDataParamBlock',['../struct_vireo_1_1_variant_to_data_param_block.html',1,'Vireo']]],
   ['vectoropinstruction',['VectorOpInstruction',['../struct_vireo_1_1_vector_op_instruction.html',1,'Vireo']]],
   ['viaformatchars',['ViaFormatChars',['../struct_vireo_1_1_via_format_chars.html',1,'Vireo']]],
   ['viaformatoptions',['ViaFormatOptions',['../struct_vireo_1_1_via_format_options.html',1,'Vireo']]],

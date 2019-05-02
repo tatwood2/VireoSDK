39921212b1a82f64158b75c1a192a904ef22a624
Vireo Bot (from Travis CI) <vireobot@users.noreply.github.com>
2019-05-02 17:19:56 -0500

    Deploy ni/VireoSDK to github.com/ni/VireoSDK.git:gh-pages

--- search/classes_3.js
+++ search/classes_3.js
@@ -6,6 +6,7 @@ var searchData=
   ['defaultpointertype',['DefaultPointerType',['../class_vireo_1_1_default_pointer_type.html',1,'Vireo']]],
   ['defaultvaluetype',['DefaultValueType',['../class_vireo_1_1_default_value_type.html',1,'Vireo']]],
   ['deletevariantattributeparamblock',['DeleteVariantAttributeParamBlock',['../struct_vireo_1_1_delete_variant_attribute_param_block.html',1,'Vireo']]],
+  ['dualtypeconversion',['DualTypeConversion',['../class_vireo_1_1_dual_type_conversion.html',1,'Vireo']]],
   ['dualtypeequal',['DualTypeEqual',['../class_vireo_1_1_dual_type_equal.html',1,'Vireo']]],
   ['dualtypeoperation',['DualTypeOperation',['../class_vireo_1_1_dual_type_operation.html',1,'Vireo']]],
   ['dualtypevisitor',['DualTypeVisitor',['../class_vireo_1_1_dual_type_visitor.html',1,'Vireo']]],
--- search/files_3.js
+++ search/files_3.js
@@ -3,6 +3,8 @@ var searchData=
   ['datareflectionvisitor_2eh',['DataReflectionVisitor.h',['../_data_reflection_visitor_8h.html',1,'']]],
   ['datatypes_2eh',['DataTypes.h',['../_data_types_8h.html',1,'']]],
   ['date_2eh',['Date.h',['../_date_8h.html',1,'']]],
+  ['dualtypeconversion_2ecpp',['DualTypeConversion.cpp',['../_dual_type_conversion_8cpp.html',1,'']]],
+  ['dualtypeconversion_2eh',['DualTypeConversion.h',['../_dual_type_conversion_8h.html',1,'']]],
   ['dualtypeequal_2ecpp',['DualTypeEqual.cpp',['../_dual_type_equal_8cpp.html',1,'']]],
   ['dualtypeequal_2eh',['DualTypeEqual.h',['../_dual_type_equal_8h.html',1,'']]],
   ['dualtypeoperation_2ecpp',['DualTypeOperation.cpp',['../_dual_type_operation_8cpp.html',1,'']]],

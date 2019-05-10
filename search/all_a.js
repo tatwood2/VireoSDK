4b0e4d911c0d2568586434644f28ea849599d54e
Vireo Bot (from Travis CI) <vireobot@users.noreply.github.com>
2019-05-10 16:12:42 -0500

    Deploy ni/VireoSDK to github.com/ni/VireoSDK.git:gh-pages

--- search/all_a.js
+++ search/all_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['javascriptref_2ecpp',['JavaScriptRef.cpp',['../_java_script_ref_8cpp.html',1,'']]],
-  ['javascriptref_2eh',['JavaScriptRef.h',['../_java_script_ref_8h.html',1,'']]]
+  ['javascriptdynamicref_2ecpp',['JavaScriptDynamicRef.cpp',['../_java_script_dynamic_ref_8cpp.html',1,'']]],
+  ['javascriptref_2eh',['JavaScriptRef.h',['../_java_script_ref_8h.html',1,'']]],
+  ['javascriptstaticref_2ecpp',['JavaScriptStaticRef.cpp',['../_java_script_static_ref_8cpp.html',1,'']]]
 ];
